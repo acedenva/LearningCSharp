bf20e3c15046ea4d3ce25b94304f50573cbc76d7
acedenva <acedenva@protonmail.com>
2023-01-04 17:19:07 +0100

    Erstes Tutorial

---
+++ helloworld.cs
@@ -0,0 +1,11 @@
+//Das Minimum, was ein C# Programm zum starten benötigt
+
+//Eine Klasse
+class myClass
+{
+	// Eine Hauptmethode
+	static void Main()
+	{
+		System.Console.WriteLine("Hallo Welt");
+	}
+}
